81ad2ab96ffe869a6c94a69c553f8a3f88ae01bb
darcyMaz <92594097+darcyMaz@users.noreply.github.com>
2022-08-09 14:08:04 -0400

    Initial commit

---
+++ src/main/java/org/dm/Electoral_Interpreter_Java_1/App.java
@@ -0,0 +1,13 @@
+package org.dm.Electoral_Interpreter_Java_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
